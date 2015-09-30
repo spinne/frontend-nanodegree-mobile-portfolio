64bc58268d48b2fac48bdb84558cafed1e83041c
Sylvia Schmidt <sylvia.schmidt@jielo.de>
2015-09-30 15:01:30 +0200

    Calculate item.length outside loop and select elements by class.

--- views/js/main.js
+++ views/js/main.js
@@ -502,8 +502,9 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
+  var items = document.getElementsByClassName('mover');
+  var len = items.length;
+  for (var i = 0; i < len; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
